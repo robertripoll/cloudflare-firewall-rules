d3704dbcdb19e8b92cbc5a76f8bdaa69b3d10143
Robert Ripoll <robriplopez@gmail.com>
2024-06-29 12:09:31 +0000

    Refactor: fragment logic into separate functions

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from cache.base import Cache
 from cache.file_cache import FileCache
 from cloudflare.base import CloudFlare
-from firewall.base import Firewall
+from firewall.base import Firewall, IPVersion, SyncableFirewall
 from firewall.firewalld import Firewalld
+
+
+def remove_old_allow_rules(ipv4: set[str], ipv6: set[str], firewall: Firewall) -> bool:
+    if not firewall.delete_allow_rules(ipv4, IPVersion.V4):
+        return False
+
+    if not firewall.delete_allow_rules(ipv6, IPVersion.V6):
+        return False
+
+    return True
+
+
+def add_new_allow_rules(ipv4: set[str], ipv6: set[str], firewall: Firewall) -> bool:
+    if not firewall.save_allow_rules(ipv4, IPVersion.V4):
+        return False
+
+    if not firewall.save_allow_rules(ipv6, IPVersion.V6):
+        return False
+
+    if issubclass(type(firewall), SyncableFirewall):
+        return firewall.sync()
+
+    return True
 
 
 def main():
@@ -18,18 +41,13 @@ def main():
     firewall: Firewall = Firewalld()
 
     if cached_etag != None:
-        cached_ipv4 = cache.get('ipv4')
-        cached_ipv6 = cache.get('ipv6')
+        cached_ipv4: set[str] = cache.get('ipv4')
+        cached_ipv6: set[str] = cache.get('ipv6')
 
-        # delete existing firewall rules
+        remove_old_allow_rules(cached_ipv4, cached_ipv6, firewall)
 
-        pass
-
-    for ip in last_ips_v4:
-        firewall.save_allow_rule(ip)
-
-    for ip in last_ips_v6:
-        firewall.save_allow_rule(ip)
+    firewall.save_allow_rules(last_ips_v4, IPVersion.V4)
+    firewall.save_allow_rules(last_ips_v6, IPVersion.V6)
 
     firewall.sync()
     cache.set("etag", last_etag)
